53fda4b84e1003c5d9a6cd48c83c77f592527d93
liubomyrzdrl <zdrilyiliubomyr@gmail.com>
2021-12-20 19:24:52 +0200

    Card test (#8)
    * after revert
    
    * befire pull
    
    * feat: add tests for Card component
    
    * fix: fix App & Card tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { MockedProvider } from '@apollo/client/testing';
 import App from './App';
+import { useIsMobile } from './components/hooks/useIsMobile';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const mocks = [];
+jest.mock('./components/hooks/useIsMobile', () => ({
+  useIsMobile: jest.fn(),
+}));
+
+describe('Test App', () => {
+  test('it should render desktop version App component', () => {
+    useIsMobile.mockImplementation(() => false);
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Router>
+          <App />
+        </Router>
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('it should render mobile version App component', () => {
+    useIsMobile.mockImplementation(() => true);
+    const { container } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Router>
+          <App />
+        </Router>
+      </MockedProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
---
+++ src/components/Card/Card.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable testing-library/no-container */
+/* eslint-disable testing-library/no-node-access */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Card } from './Card';
+
+describe('Test Card', () => {
+  it('should render Card component without error', () => {
+    const { container } = render(<Card />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render Card component element by class "card"', () => {
+    const { container } = render(<Card />);
+    expect(container.getElementsByClassName('card')[0]).toBeInTheDocument();
+  });
+
+  it('should render Card component element by text child "Test"', () => {
+    render(<Card>Test</Card>);
+    expect(screen.getByText('Test')).toBeInTheDocument();
+  });
+
+  it('should render Card component element by props className - "card-mobile"', () => {
+    const { container } = render(<Card className="card-mobile" />);
+    expect(container.getElementsByClassName('card-mobile')[0]).toBeInTheDocument();
+  });
+});
